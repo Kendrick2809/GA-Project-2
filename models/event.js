3abca0b6fce242a4c1b57fc8ac428702051182e2
Kendrick Winata <kendrickwinata@Najwas-MacBook-Pro-2.local>
2022-08-06 09:23:16 +0800

    first commit

---
+++ models/event.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
